b195c0c72dc53f90c5bfa86b54a4bddb1cb42873
18nanma <42802604+18nanma@users.noreply.github.com>
2020-02-13 11:41:53 +0530

    program 2

---
+++ bubble_sort.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+#include <bits/stdc++.h> 
+using namespace std;
+void bubblesort(int arr[],int n)
+{	int temp;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+		cout<<arr[i]<<"\t";
+}
+
+int main()
+{	int n,ch;
+	clock_t start, end; ;
+	cout<<"Enter number of elements:"<<endl;
+	cin>>n;
+	int arr[n];
+	cout<<"Enter the array elements:"<<endl;
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	
+	//bubble sort - time complexity
+	start = clock(); 
+	 bubblesort(arr,n);
+	end = clock(); 
+ 		
+	
+	double time_taken = double(end - start) / double(CLOCKS_PER_SEC); 
+    cout << "Time taken by program is : " << fixed  
+         << time_taken << setprecision(5); 
+    cout << " sec " << endl; 
+   
+	return 0;
+}
